82d2d600f5da545eaba8130f008755e80bb17706
Pavitar Dua <pavitardua@Mobiuso-iMac3.local>
2018-05-08 11:19:49 +0530

    corrected connection string

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 // mongoose.connect('mongodb://localhost:27017/TodoApp');
-mongoose.connect('mongodb://<pavitar>:<vicsach>@ds217310.mlab.com:17310/notes-todo-api');
+mongoose.connect('mongodb://pavitar:vicsach@ds217310.mlab.com:17310/notes-todo-api');
 // mongodb://<dbuser>:<dbpassword>@ds217310.mlab.com:17310/notes-todo-api
 module.exports.mongoose = {
     mongoose
