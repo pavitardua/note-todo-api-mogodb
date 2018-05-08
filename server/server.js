16575084763a7dd59d2c3d578cc92bdaeb686249
Pavitar Dua <pavitardua@Mobiuso-iMac3.local>
2018-05-08 10:33:03 +0530

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const {ObjectID} = require('mongodb');
+
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
@@ -43,6 +45,18 @@ app.post('/users',(req,res)=>{
           res.status(400).send(e);
     });
   });
+//GET /todos/
+app.get('/todos/:id',(req,res)=>{
+    var id = req.params.id;
+    if(!ObjectID.isValid(id)){
+        return res.status(404).send();
+    }
+    Todo.findById(id).then((todo)=>{
+        res.send({todo});
+    },(e)=>{
+        res.status(400).send(e);
+    });
+});
 
 app.listen(3000,()=>{
     console.log('Started on port 3000');
