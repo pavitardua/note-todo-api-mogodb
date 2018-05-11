878682472d42ac5486a8187ec9d97302095f38d2
Pavitar Dua <pavitardua@Mobiuso-iMac3.local>
2018-05-11 14:55:37 +0530

    Added remove

---
+++ playground/mongoose-remove.js
@@ -0,0 +1,10 @@
+const {ObjectID} = require('mongodb');
+
+const {mongoose} = require('./../server/db/mongoose');
+const {Todo} = require('./../server/models/todo');
+const {User} = require('./../server/models/user');
+
+//remove all documents
+Todo.remove({}).then((result)=>{
+    console.log(result);
+});
